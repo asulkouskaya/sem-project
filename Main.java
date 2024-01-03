06268e4e061a6dfd95a97ed2a7e3c1c285533fa6
Helia <124560143+asulkouskaya@users.noreply.github.com>
2024-01-04 00:16:56 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
 package com.example.semproj;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Window chooser = new Window("Semester project: calculations");
-        chooser.pack();
-        chooser.setSize(800, 400);
-        chooser.setLocation(400, 50);
-        chooser.setVisible(true);
+        int interface_;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("Введите 1, если хотите использовать GUI; введите 2, если хотите спользовать Web-interface: ");
+            interface_ = in.nextInt();
+        } while (interface_ != 1 && interface_ != 2);
+
+        if (interface_ == 1) {
+            Window chooser = new Window("Semester project: calculations");
+            chooser.pack();
+            chooser.setSize(800, 400);
+            chooser.setLocation(250, 120);
+            chooser.setVisible(true);
+        }
+
     }
 }
--- Window.java
+++ Window.java
@@ -19,23 +19,28 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Window extends JFrame {
 
+    private ArrayList<String> strings;
+    private HandlingStrategy method;
     private final JTextArea inputArea;
     private final JTextArea outputArea;
 
     Window(String name) {
         super(name);
+        strings = new ArrayList<>();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
         inputArea = new JTextArea();
         add(inputArea);
         inputArea.setEditable(false);
-        inputArea.setColumns(25);
-        inputArea.setBackground(Color.white);
-        inputArea.setBorder(BorderFactory.createLineBorder(Color.gray, 1));
+
+        JButton read = new JButton("Read");
+        add(read);
+
+        JButton write = new JButton("Write");
+        add(write);
 
         JRadioButton txtWrite = new JRadioButton(".txt");
         add(txtWrite);
@@ -49,13 +54,9 @@ public class Window extends JFrame {
         outputArea = new JTextArea();
         add(outputArea);
         outputArea.setEditable(false);
-        outputArea.setColumns(25);
-        outputArea.setBackground(Color.white);
-        outputArea.setBorder(BorderFactory.createLineBorder(Color.gray, 1));
-
 
         read.addActionListener(e -> {
-            JFileChooser fileChooser = new JFileChooser(".");
+            JFileChooser fileChooser = new JFileChooser("F:\\BSU-FAMCs\\2-course\\IP\\sem-proj\\src\\main\\java\\com\\example\\semproj\\inputs");
             try {
                 if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                     if (fileChooser.getSelectedFile().getName().endsWith("txt")) {
@@ -71,18 +72,86 @@ public class Window extends JFrame {
                 JOptionPane.showMessageDialog(null, exception.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
             }
             showInput();
+            solveProblems();
+            showOutput();
         });
+
+        write.addActionListener(e -> {
+            try {
+                String dir = "F:\\BSU-FAMCs\\2-course\\IP\\sem-proj\\src\\main\\java\\com\\example\\semproj\\outputs\\";
+                String fileName = JOptionPane.showInputDialog("Enter file name you want to save result to");
+                if (fileName.isEmpty()) throw new IllegalArgumentException("File name is empty.");
+                if (txtWrite.isSelected()){
+                    writeToTXT(dir + fileName + ".txt");
+                } else if (xmlWrite.isSelected()){
+                    writeToXML(dir + fileName + ".xml");
+                }
+                JOptionPane.showMessageDialog(null, "File was written successfully!");
+            } catch (IOException | ParserConfigurationException | TransformerException |
+                     IllegalArgumentException exception) {
+                JOptionPane.showMessageDialog(null, exception.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+
+    private void writeToTXT(String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        for (String string : strings) {
+            fileWriter.write(string);
+            fileWriter.write("\n");
+        }
+        fileWriter.close();
+    }
+
+    private void writeToXML(String fileName) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document document = factory.newDocumentBuilder().newDocument();
+        Element root = document.createElement("text");
+        document.appendChild(root);
+        for (String string : strings) {
+            Element element = document.createElement("string");
+            element.setTextContent(string);
+            root.appendChild(element);
+        }
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(new DOMSource(document), new StreamResult(new File(fileName)));
+    }
+
+    private void solveProblems() {
+        Pattern p = Pattern.compile("(\\s*-?\\s*(\\(*\\s*(-?\\s*\\d+(\\.\\d+)?)\\s*\\)*\\s*)*[-+*/^](\\s*\\(*\\s*(-?\\s*\\d+(\\.\\d+)?)\\s*\\)*\\s*)+)+");
+        String oldSubstring;
+        String newSubstring;
+        for (int i = 0; i < strings.size(); i++) {
+            Matcher matcher = p.matcher(strings.get(i));
+            while (matcher.find()) {
+                oldSubstring = strings.get(i).substring(matcher.start(), matcher.end());
+                newSubstring = SmartCalculator.calculate(oldSubstring);
+                strings.set(i, strings.get(i).replace(oldSubstring, newSubstring));
+            }
+        }
+
     }
 
     private void setStrategy(HandlingStrategy handlingStrategy) {
         method = handlingStrategy;
     }
 
-     private void showInput() {
+    private void showInput() {
         StringBuilder stringBuilder = new StringBuilder();
         for (String string : strings) {
             stringBuilder.append(string).append("\n");
         }
         inputArea.setText(stringBuilder.toString());
+    }
+
+    private void showOutput() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strings) {
+            stringBuilder.append(string).append("\n");
+        }
+        outputArea.setText(stringBuilder.toString());
     }
 }
